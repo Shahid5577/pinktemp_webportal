d63de58b57a98de750b60a70366fd5810ec63c4e
Shahid5577 <shashaik673@gmail.com>
2025-02-18 17:09:12 +0530

    Update On Security

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,9 +6,34 @@ import About from '../src/pages/About';
 import Services from '../src/pages/Services';
 import Pricing from '../src/pages/Pricing';
 import Contact from './pages/Contact';
-import Home from './components/Brochure'; 
+import Home from './components/Brochure';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const preventRightClick = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
+    const preventDevTools = (e: KeyboardEvent) => {
+      if (
+        e.keyCode === 123 ||
+        (e.ctrlKey && e.shiftKey && e.keyCode === 73) ||
+        (e.ctrlKey && e.shiftKey && e.keyCode === 74) ||
+        (e.ctrlKey && e.keyCode === 85)
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", preventRightClick);
+    window.addEventListener("keydown", preventDevTools);
+
+    return () => {
+      document.removeEventListener("contextmenu", preventRightClick);
+      window.removeEventListener("keydown", preventDevTools);
+    };
+  }, []);
+
   return (
     <Router>
       <Header />
@@ -19,7 +44,7 @@ const App: React.FC = () => {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
